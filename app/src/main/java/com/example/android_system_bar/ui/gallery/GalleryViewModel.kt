31e3864ca0cdf28f58204ab84df1b2d6ca5a8b16
vitorfgodoy <vitorfgodoy@gmail.com>
2021-08-26 11:17:56 -0300

    first commit

---
+++ app/src/main/java/com/example/android_system_bar/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.android_system_bar.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
